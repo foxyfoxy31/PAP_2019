0fa5e3ba7b87c135d4f4205f8cf4578a26928017
foxyfoxy31 <danielmacedo02@gmail.com>
2019-11-14 11:16:46 +0000

    Dialogue system progress

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
+    private Queue<string> sentences; 
+    private PlayerController2D player;
+    void Start()
+    {
+        sentences = new Queue<string>();
+        player = FindObjectOfType<PlayerController2D>();
+    }
+
+    public void StartDialogue (Dialogue dialogue) {
+
+        player.rb2d.velocity = new Vector2 (0, 0);
+
+        player.enabled = false; 
+
+        animator.SetBool("isOpen", true);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences) {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence () {
+        if (sentences.Count == 0) {
+            EndDialogue();
+            return;
+        }
+
+        string  sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence) {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray()) {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+    void EndDialogue() {
+        animator.SetBool("isOpen", false);
+        player.enabled = true; 
+    }
+}
---
+++ Assets/Scripts/TriggerDialogueOnKey.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDialogueOnKey : MonoBehaviour
+{
+    private bool dialogueTriggered = false;
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if (other.gameObject.name == "lucas") {
+            if(Input.GetKeyDown("m") && !dialogueTriggered) {
+                gameObject.GetComponent<DialogueTrigger>().TriggerDialogue();
+                dialogueTriggered = true;
+            }
+            else if (Input.GetKeyUp("m")) {
+                FindObjectOfType<DialogueManager>().DisplayNextSentence();
+            }
+            else if (FindObjectOfType<PlayerController2D>().enabled == true) {
+                dialogueTriggered = false;
+            }
+        }
+    }
+}
