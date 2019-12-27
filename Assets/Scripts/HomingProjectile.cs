fca54bdd7a451a62491a098866c88fdb5ac34525
foxyfoxy31 <danielmacedo02@gmail.com>
2019-12-27 20:38:18 +0000

    Following Orb

---
+++ Assets/Scripts/HomingProjectile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class HomingProjectile : MonoBehaviour
+{
+
+    public Transform target;
+    public float speed = 1f;
+    public float rotateSpeed = 100f;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 direction = (Vector2)target.position - rb.position;
+        direction.Normalize();
+        float rotateAmount = Vector3.Cross(direction, transform.up).z;
+        rb.angularVelocity = -rotateAmount * rotateSpeed;
+        rb.velocity = transform.up * speed;
+
+
+    }
+}
