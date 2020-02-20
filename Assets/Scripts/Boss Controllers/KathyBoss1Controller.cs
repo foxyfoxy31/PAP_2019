63f79b65e4986cfa484ea016b08ed498d3591367
foxyfoxy31 <danielmacedo02@gmail.com>
2020-02-20 11:07:20 +0000

    Boss to Database Menu
    I can now put actual data in the leaderboard!
    Yay! :D

--- Assets/Scripts/Boss Controllers/KathyBoss1Controller.cs
+++ Assets/Scripts/Boss Controllers/KathyBoss1Controller.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class KathyBoss1Controller : BossController
 {   
@@ -42,6 +43,7 @@ public class KathyBoss1Controller : BossController
                 if (bossHealthManager.isDead == true) {
                     idle();
                     active = false;
+                    SceneManager.LoadScene("DatabaseMenu");
                 }
                 else if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !animator.IsInTransition(0)) {
                     animationState = Random.Range(1, 4);
