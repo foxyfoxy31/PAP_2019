3f2f65b1b5e5664cf593853082aa10765f0fa376
foxyfoxy31 <danielmacedo02@gmail.com>
2020-03-01 23:29:47 +0000

    Created leaderboard script and added more entries

---
+++ Assets/Scripts/Leaderboard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leaderboard : MonoBehaviour
+{
+    private Transform entryContainer;
+    private Transform entryTemplate;
+    private void Awake() {
+        entryContainer = transform.Find("leaderboardEntryContainer");
+        entryTemplate = entryContainer.Find("leaderboardEntryTemplate");
+    }
+}
