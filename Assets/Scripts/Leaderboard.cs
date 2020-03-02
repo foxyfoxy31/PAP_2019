27299c2fb95875364019794c98d2027865b0f7cb
foxyfoxy31 <danielmacedo02@gmail.com>
2020-03-02 12:48:21 +0000

    Progress on Leadeboard script

--- Assets/Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard.cs
@@ -1,13 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Leaderboard : MonoBehaviour
 {
     private Transform entryContainer;
     private Transform entryTemplate;
+    
     private void Awake() {
         entryContainer = transform.Find("leaderboardEntryContainer");
         entryTemplate = entryContainer.Find("leaderboardEntryTemplate");
+        entryTemplate.gameObject.SetActive(false);
+
+        float templateHeight = 30f;
+        for (int i = 0; i < 10; i++) {
+            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
+            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
+            entryTransform.gameObject.SetActive(true);
+
+            int rank = i + 1;
+            string rankString;
+            switch (rank) {
+                default:
+                    rankString = rank + "TH";
+                break;
+
+                case 1: rankString = "1ST"; break;
+                case 2: rankString = "2ND"; break;
+                case 3: rankString = "3RD"; break;
+            }
+
+            entryTransform.Find("posText").GetComponent<Text>().text = rankString;
+        
+            float time = Random.Range(0f, 200f);
+
+            string minutes = ((int) time / 60).ToString();
+            string seconds = (time % 60).ToString("f2");
+
+            entryTransform.Find("timeText").GetComponent<Text>().text = minutes + ":" + seconds;
+
+        }
+
     }
+
+    private void CreateLeaderboardEntryTransform(LeaderboardEntry leaderboardEntry, Transform container, List<Transform> transformList) {
+
+    }
+
+    private class LeaderboardEntry {
+        public float time;
+        public string name;
+    }
+
 }
