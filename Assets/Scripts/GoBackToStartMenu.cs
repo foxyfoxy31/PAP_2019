86edee031c5261f3b6a62a6e29afd0791d6bdb8f
foxyfoxy31 <danielmacedo02@gmail.com>
2020-03-04 13:16:58 +0000

    Back To Menu Button

---
+++ Assets/Scripts/GoBackToStartMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoBackToStartMenu : MonoBehaviour
+{
+    public void GoToStartMenu() {
+        Debug.Log("Going back!");
+        SceneManager.LoadScene("MainMenu");
+    }
+}
